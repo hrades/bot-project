d6cb22ed604a84f229931fa2b0adebe93930ad59
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-01-30 18:30:15 -0300

    Update display.launch.py

--- examples/launch/display.launch.py
+++ examples/launch/display.launch.py
@@ -8,7 +8,7 @@ from ament_index_pyhton.packages import get_package_share_path
 
 def generate_launch_description():
 
-    urdf_path = os.path.join(get_package_share_path('description'), 'urdf', 'my_robot.urdf')
+    urdf_path = os.path.join(get_package_share_path('description'), 'my_robot.urdf')
 
     robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
 
