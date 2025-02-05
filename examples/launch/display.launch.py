2412665e747d3c0e98e6971f6f4e467dbf29df74
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-02-05 10:50:26 -0300

    Update display.launch.py
    Mudança de arquivo .urdf

--- examples/launch/display.launch.py
+++ examples/launch/display.launch.py
@@ -8,7 +8,7 @@ from ament_index_pyhton.packages import get_package_share_path
 
 def generate_launch_description():
 
-    urdf_path = os.path.join(get_package_share_path('description'), 'my_robot.urdf')
+    urdf_path = os.path.join(get_package_share_path('description'), 'my_robot.urdf.xacro')
 
     rviz_path = os.path.join(get_package_share_path('description'), 'urdf_robot_config.rviz')
 
