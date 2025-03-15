bd0b9ba953628d20051d5d52b90c280d0a678300
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-03-15 16:26:28 -0300

    Add Gazebo Classic files

---
+++ my_robot_bringup/launch/robot_gazebo.launch.py
@@ -0,0 +1,86 @@
+from os.path import join
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+from launch.actions import AppendEnvironmentVariable
+
+
+
+def generate_launch_description():
+    
+    '''joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )'''
+
+    gazebo_params_file = join(get_package_share_directory('my_robot_description'), 'config', 'gazebo_params.yaml')
+    
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
+    
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_main_robot'])
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+
+    # Code for delaying a node (I haven't tested how effective it is)
+    # 
+    # First add the below lines to imports
+    # from launch.actions import RegisterEventHandler
+    # from launch.event_handlers import OnProcessExit
+    #
+    # Then add the following below the current diff_drive_spawner
+    # delayed_diff_drive_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=spawn_entity,
+    #         on_exit=[diff_drive_spawner],
+    #     )
+    # )
+    #
+    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        AppendEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=join(get_package_share_directory('my_robot_description'), "models")),
+        #joystick,
+        #twist_mux,
+        #gazebo,
+        #spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
+    ])
