22c6ed7caa6eb7a7c6b5d86fc5079c3504018ff0
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-03-18 19:29:09 -0300

    Add joystick launch file
    Arquivo para rodar as configurações do controle (joystick)

---
+++ my_robot_bringup/launch/joystick.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    package = 'my_robot_description'
+    joy_params = os.path.join(get_package_share_directory(package),'config','joystick.yaml')
+
+    joy_node = Node(
+            package='joy',
+            executable='joy_node',
+            parameters=[joy_params, {'use_sim_time': use_sim_time}],
+         )
+
+    teleop_node = Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            name='teleop_node',
+            parameters=[joy_params, {'use_sim_time': use_sim_time}],
+            remappings=[('/cmd_vel','/cmd_vel_joy')]
+         )
+
+    return LaunchDescription([
+        joy_node,
+        teleop_node
+    ])
