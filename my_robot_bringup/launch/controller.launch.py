0798fee757ed92bc4b53cd69a9ef32a8fc27e320
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-07-09 15:43:40 -0300

    Update controller.launch.py
    changed features

--- my_robot_bringup/launch/controller.launch.py
+++ my_robot_bringup/launch/controller.launch.py
@@ -5,20 +5,39 @@ from launch.actions import  TimerAction
 from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
+from launch_ros.parameter_descriptions import ParameterValue
 
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    robot_description = ParameterValue(
+        Command(
+            [
+                "xacro ",
+                os.path.join(
+                    get_package_share_directory("my_robot_description"),
+                    "urdf",
+                    "my_main_robot.urdf.xacro",
+                ),
+                " use_sim:=False"
+            ]
+        ),
+        value_type=str,
+    )
     
-    controller_params_file = os.path.join(get_package_share_directory('my_robot_description'),'config','robot_controllers.yaml')
-
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
-                    controller_params_file]
+        parameters=[
+            {"robot_description": robot_description,
+             "use_sim_time": False},
+            os.path.join(
+                get_package_share_directory("my_robot_description"),
+                "config",
+                "robot_controllers.yaml",
+            ),
+        ],
     )
 
     diff_drive_spawner = Node(
