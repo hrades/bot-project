e59b8824c1190409262abf7f42476fca43cd90a6
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-08-05 19:40:06 -0300

    Update robot_controller.ino
    Added test code

--- my_robot_firmware/arduino/robot_controller/robot_controller.ino
+++ my_robot_firmware/arduino/robot_controller/robot_controller.ino
@@ -42,11 +42,11 @@ double left_wheel_meas_vel = 0.0;     // rad/s
 double right_wheel_cmd = 0.0;             // 0-255
 double left_wheel_cmd = 0.0;              // 0-255
 // Tuning
-double Kp_r = 6.0; //11.5;
-double Ki_r = 1.0; //7.5;
+double Kp_r = 8.95; //11.5;
+double Ki_r = 0.0; //7.5;
 double Kd_r = 0.0; //0.1;
-double Kp_l = 6.2; //12.8;
-double Ki_l = 1.0; //8.3;
+double Kp_l = 10.5; //12.8;
+double Ki_l = 0.0; //8.3;
 double Kd_l = 0.0; //0.1;
 // Controller
 PID rightMotor(&right_wheel_meas_vel, &right_wheel_cmd, &right_wheel_cmd_vel, Kp_r, Ki_r, Kd_r, DIRECT);
@@ -187,8 +187,13 @@ void loop() {
       left_wheel_cmd = 0.0;
     }
 
-    String encoder_read = "r" + right_wheel_sign + String(right_wheel_meas_vel) + ",l" + left_wheel_sign + String(left_wheel_meas_vel) + ",";
-    Serial.println(encoder_read);
+    String encoder_read = "r" + right_wheel_sign + String(right_wheel_meas_vel) + ",l" + left_wheel_sign + String(left_wheel_meas_vel);
+    String encoder_plotter = "direito:"+String(right_wheel_meas_vel) + ",esquerdo:"+String(left_wheel_meas_vel);
+    String setpoints = ",set_dir:"+String(right_wheel_cmd_vel)+",set_esq:"+String(left_wheel_cmd_vel)+",";
+    //Serial.print(encoder_read); --> Utilizar no código do robô
+    //Para testes com serial plotter abaixo
+    Serial.print(setpoints); 
+    Serial.println(encoder_plotter);
     last_millis = current_millis;
     right_encoder_counter = 0;
     left_encoder_counter = 0;
