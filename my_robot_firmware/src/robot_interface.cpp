7d9e373f912de03e0c0a8f9a96cee05ce02458fe
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-09-17 19:21:19 -0300

    Update robot_interface.cpp

--- my_robot_firmware/src/robot_interface.cpp
+++ my_robot_firmware/src/robot_interface.cpp
@@ -79,7 +79,7 @@ CallbackReturn RobotInterface::on_activate(const rclcpp_lifecycle::State &)
     try
     {
       arduino_.Open(port_);
-      arduino_.SetBaudRate(LibSerial::BaudRate::BAUD_115200);
+      arduino_.SetBaudRate(LibSerial::BaudRate::BAUD_57600);
     }
     catch (...)
     {
